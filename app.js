57aa1b51309f527211f5d70b500eb9f7821ccd1a
Jorge Ciro Brandan <jorgebrandan31@yahoo.com.ar>
2025-02-14 20:02:06 -0300

    Se creo la Funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = []
+let amigos = [];
+console.log(amigos)
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo === '') {
+        alert('El campo está vacío, debe escribir un nombre.');
+    } else {
+        amigos.push(nombreAmigo);
+    }
+    console.log(amigos)
+    limpiarCaja();
+    return;
+    
+}   
 
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
