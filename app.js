f44bd284e193111e049ebd2683e7db71debd2213
Jorge Ciro Brandan <jorgebrandan31@yahoo.com.ar>
2025-02-22 08:39:45 -0300

    se crea la funcion actualizarListaDeAmigos

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let listaDeAmigos = document.getElementById('listaAmigos');
 console.log(amigos)
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
@@ -11,10 +12,20 @@ function agregarAmigo() {
         amigos.push(nombreAmigo);
     }
     console.log(amigos)
+    actualizarListaDeAmigos();
     limpiarCaja();
-    return;
-    
-}   
+    return;    
+}  
+
+function actualizarListaDeAmigos() {
+    listaDeAmigos.innerHTML = '';
+    for (var i = 1; i <= amigos.length; i++) {
+        let nombreAgregado = document.createElement('li');
+        nombreAgregado.textContent = ` ${amigos[i - 1]}`;
+        listaDeAmigos.appendChild(nombreAgregado);
+    }
+}
+
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
