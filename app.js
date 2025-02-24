32ac5878f08bb9a739231c17ba0329868544da11
Jorge Ciro Brandan <jorgebrandan31@yahoo.com.ar>
2025-02-24 14:15:55 -0300

    Se crea la funcion nuevaLista

--- app.js
+++ app.js
@@ -4,15 +4,18 @@
 let amigos = [];
 let listaDeAmigos = document.getElementById('listaAmigos');
 let amigoSorteado = document.getElementById('resultado');
-console.log(amigos)
+let reiniciar = document.getElementById('reiniciar');
+
+
+
 function agregarAmigo() {
+    
     let nombreAmigo = document.getElementById('amigo').value;
     if (nombreAmigo === '') {
         alert('El campo está vacío, debe escribir un nombre.');
     } else {
         amigos.push(nombreAmigo);
-    }
-    console.log(amigos)
+    }       
     actualizarListaDeAmigos();
     limpiarCaja();
     return;    
@@ -28,16 +31,28 @@ function actualizarListaDeAmigos() {
 }
 
 function sortearAmigo() {
+
     if (amigos.length >= 2) {
         let amigoAleatorio = Math.floor(Math.random() * amigos.length);
         amigoSorteado.innerHTML = `El amigo sorteado es: ${amigos[amigoAleatorio]}.`;
     } else {
-        alert('Debe añadir al menos un nombre mas.');
-    }
+        alert('Debe añadir al menos dos nombres.');
+    } 
+
 }
-
-
 
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
 }
+
+function nuevaLista() {
+    if (amigos.length !='') {
+    location.reload();
+    amigos.splice(0,amigos.length);
+    } else {
+        alert('La lista se encuentra vacia.');
+    }
+    
+}
+
+
